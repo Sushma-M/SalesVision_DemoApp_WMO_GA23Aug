1bd38823438463e2e6dfc8b4ef1a1ba313e69427
test test <test@wavemaker.com>
2018-08-22 01:35:46 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "clear-glass",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_DemoApp_WMO",
+  "homePage" : "Main",
+  "name" : "SalesVision_DemoApp_WMO",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
